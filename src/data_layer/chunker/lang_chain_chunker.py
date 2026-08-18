06b17a56207699ea8308dcf1a3c99335d0406f6d
seespinoza <se.espinoza132@gmail.com>
2026-08-18 14:24:15 -0400

    added return types

--- src/data_layer/chunker/lang_chain_chunker.py
+++ src/data_layer/chunker/lang_chain_chunker.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 def markdown_chunk(
     text: str, headers_to_split_on: list[tuple[str, str]], strip_headers: bool = False
-):
+) -> None:
     return ("", {})
 
 
 def fixed_size_chunk(
     text: str, chunk_size: int, chunk_overlap: int, length_function: str
-):
+) -> None:
     return ("", "")
 
 
